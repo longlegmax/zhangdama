ce0b0683ea684d3e2c44f75d9e9cba0c1aef9dd0
NatsukiKoo <39581263+AlterGu@users.noreply.github.com>
2021-09-21 10:35:26 +0800

    Update smzdm.js

--- function/smzdm.js
+++ function/smzdm.js
@@ -18,7 +18,7 @@ const SEND_KEY = process.env.SEND_KEY
 async function downFile () {
     // const url1 = 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/backup/smzdm_checkin.js'
     // const url1 = 'https://raw.githubusercontent.com/jiegto/Actions_smzdm/main/backup/smzdm_checkin.js'
-    const url1 = 'https://raw.githubusercontent.com/AlterGu/smzdm/main/smzdm_checkin.js'
+    const url1 = 'https://raw.githubusercontent.com/Tsukasa007/my_script/master/smzdm_mission.js'
     const url2 = 'https://raw.githubusercontent.com/jiegto/Actions_smzdm/main/function/magic.json'    
     await download(url1, './')
     await download(url2, './')
